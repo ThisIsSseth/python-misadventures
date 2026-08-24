b9127ce86544ed3067bb271d71045d6c79e69d76
ThisIsSseth <setayesh.ziyaee@gmail.com>
2026-08-24 19:29:59 +0330

    beginner conditional statement notes

---
+++ hello/conditionalStatements.py
@@ -0,0 +1,29 @@
+# and is just and and or is just or 😃
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_toppings) #prints true
+print('mushrooms' not in requested_toppings) #prints false
+
+b = False
+c = False
+for a in requested_toppings:
+    if a == 'mushrooms':
+     b = True
+    elif a == 'onions':
+     c = True
+if (b == True and c == True):
+    print("Ooh! Both mushrooms and onions! yum yum!")
+
+# if condition:
+#     statement
+# elif condition2:
+#     statement2
+# else:
+#     statement3
+
+if requested_toppings: #returns false if lisst is empty
+    x = 5
+
+
+
+
