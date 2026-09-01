853c7a19335772eb0042c5d279a2ba6c8310b8c9
ThisIsSseth <setayesh.ziyaee@gmail.com>
2026-09-01 23:40:57 +0330

    intro to pass, match and shorthands and loops

--- hello/conditionalStatements.py
+++ hello/conditionalStatements.py
@@ -1,5 +1,5 @@
 # and is just and and or is just or 😃
-
+# Python evaluates not first, then and, then or.
 requested_toppings = ['mushrooms', 'onions', 'pineapple']
 print('mushrooms' in requested_toppings) #prints true
 print('mushrooms' not in requested_toppings) #prints false
@@ -21,9 +21,80 @@ if (b == True and c == True):
 # else:
 #     statement3
 
-if requested_toppings: #returns false if lisst is empty
-    x = 5
+if requested_toppings: #returns false if list is empty
+    pass #just nothing
+'''
+Any string is True, except empty strings.
+Any number is True, except 0.
+Any list, tuple, set, and dictionary are True, except empty ones.
+'''
+#-----____ elif____-----
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+#short hand if
+if a > b: print("a is greater than b") 
+
+#short hand if else
+print("A") if a > b else print("B") 
 
 
+#----------
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({}) 
+#all are false
+
+# ------------_____ match_____---------
+day = 4
+match day:
+    case 1 | 2 | 3 | 4 | 5:
+        print("Today is a weekday")
+    case 6 | 7:
+        print("I love weekends!")
+    case _:
+        pass
 
 
+month = 5
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5 if month == 4:
+    print("A weekday in April")
+  case 1 | 2 | 3 | 4 | 5 if month == 5:
+    print("A weekday in May")
+  case _:
+    print("No match")
+
+
+#_____ loops
+
+# With the continue statement we can stop the current iteration,
+# and continue with the next:
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1 
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
