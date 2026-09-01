8739fad58ac21f9d0674d3c474feb8ca3d5329a7
ThisIsSseth <setayesh.ziyaee@gmail.com>
2026-09-01 23:39:16 +0330

    smol notes

--- hello/tuplesAndDictionaries.py
+++ hello/tuplesAndDictionaries.py
@@ -29,10 +29,18 @@ thistuple = ("apple")
 # empty tuple 
 thistuple = ()
 
+#multiplying a tuple
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)
+
 for dimension in dimensions:
     print(dimension) #returns all the elements in the tuple like in a list
 #400
 #100
+
+# .count()	Returns the number of times a specified value occurs in a tuple
+# .index()
 
 
 #----____dictionary ____-------------------------
@@ -70,3 +78,45 @@ for language in set(favorite_languages.values()):
 
 # nesting: to store a set of dictionaries in a list or a list of items 
 # as a value in a dictionary.
+
+# The items() method will return each item in a dictionary, as tuples in a list.
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020}) 
+
+for x in thisdict.keys():
+  print(x) 
+
+for x in thisdict:
+  print(x) 
+  #keys
+
+
+for x in thisdict.values():
+  print(x) 
+
+for x in thisdict:
+  print(thisdict[x]) 
+  #values
+
+for x, y in thisdict.items():
+  print(x, y) 
+
+'''
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+'''
+
